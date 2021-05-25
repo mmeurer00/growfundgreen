5d6429b5347092490fed4f763fbc7d3bc6446872
Maxine <maxinemeurer@yahoo.com[remote origin]>
2021-05-25 11:55:45 -0400

    create controllers and namespace

--- app/controllers/api/v1/campaigns_controller.rb
+++ app/controllers/api/v1/campaigns_controller.rb
@@ -3,40 +3,41 @@ class Api::V1::CampaignsController < ApplicationController
 
     # GET /campaigns
     def index
+        
         @campaigns = Campaign.all
-
+       
         render json: @campaigns
     end
 
-    # GET /campaigns/1
-        # def show
-        #   render json: @campaign
-    # end
+    #GET /campaigns/1
+        def show
+          render json: @campaign
+    end
 
-    # # POST /campaigns
-    # def create
-    #   @campaign = Campaign.new(campaign_params)
+    # POST /campaigns
+    def create
+      @campaign = Campaign.new(campaign_params)
 
-    #   if @campaign.save
-    #     render json: @campaign, status: :created, location: @campaign
-    #   else
-    #     render json: @campaign.errors, status: :unprocessable_entity
-    #   end
-    # end
+      if @campaign.save
+        render json: @campaign, status: :created, location: @campaign
+      else
+        render json: @campaign.errors, status: :unprocessable_entity
+      end
+    end
 
-    # # PATCH/PUT /campaigns/1
-    # def update
-    #   if @campaign.update(campaign_params)
-    #     render json: @campaign
-    #   else
-    #     render json: @campaign.errors, status: :unprocessable_entity
-    #   end
-    # end
+    # PATCH/PUT /campaigns/1
+    def update
+      if @campaign.update(campaign_params)
+        render json: @campaign
+      else
+        render json: @campaign.errors, status: :unprocessable_entity
+      end
+    end
 
-    # # DELETE /campaigns/1
-    # def destroy
-    #   @campaign.destroy
-    # end
+    # DELETE /campaigns/1
+    def destroy
+      @campaign.destroy
+    end
 
     private
     # Use callbacks to share common setup or constraints between actions.
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+#saveTheWhales = Campaign.create(name: "Save the Whales", description: "Stop whaling", goal: "50,000")
+
+saveTheSharks = Campaign.create(name: "Save the Sharks", description: "Stop shark fining", goal: "50,000")
+
+saveTheOcean = Campaign.create(name: "Save the Ocean", description: "Stop plastic pollution", goal: "500,000")
+
+saveTheOcean.save
+#saveTheWhales.save
+saveTheSharks.save
